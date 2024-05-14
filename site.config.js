198ccaedc552c765b1770fba6d6e03da70296f25
jijiang hou <113097833+daheitian@users.noreply.github.com>
2024-05-14 17:15:32 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,14 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "好室咨询",
-    image:"<div style="position: relative; width: 100%; height: 0; padding-top: 100.0000%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-    src="https:&#x2F;&#x2F;www.canva.cn&#x2F;design&#x2F;DAFY1NHHl4s&#x2F;7bFlUrw91MfjVafL5Gm4dw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
-<a href="https:&#x2F;&#x2F;www.canva.cn&#x2F;design&#x2F;DAFY1NHHl4s&#x2F;7bFlUrw91MfjVafL5Gm4dw&#x2F;view?utm_content=DAFY1NHHl4s&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">好宅</a>，", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image:"/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "hooozzz",
     bio: "一个优秀的室内设计咨询服务平台，为您和您的家庭找到最合适的设计企业",
     email: "daheitian@foxmail.com",
